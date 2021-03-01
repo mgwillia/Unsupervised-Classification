ee70e6fe58abccac4e1b1bd666dec626ad882d4d
mgwillia <mattgwilliamjr@byu.edu>
2021-03-01 00:32:52 -0500

    set sbatch

--- pascal_pretrained_simclr.sh
+++ pascal_pretrained_simclr.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=pascal_pretrained_simclr                                 # sets the job name
 #SBATCH --output=pascal_pretrained_simclr.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
 #SBATCH --error=pascal_pretrained_simclr.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
-#SBATCH --time=24:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --qos=medium                                           # set QOS, this will determine what resources can be requested
-#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --qos=high                                           # set QOS, this will determine what resources can be requested
+#SBATCH --gres gpu:p6000:4
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
