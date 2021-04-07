5bbf532a1b90d4a83e402b3200b17c274f661090
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-04-07 19:07:30 -0400

    add selflabel cifar

---
+++ selflabel_cifar10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=selflabel_cifar10                                 # sets the job name
+#SBATCH --output=selflabel_cifar10.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=selflabel_cifar10.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --time=24:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --qos=medium                                           # set QOS, this will determine what resources can be requested
+#SBATCH --gres=gpu:2
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+
+module load cuda/10.0.130                                    # run any commands necessary to setup your environment
+
+srun bash -c "hostname; python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml"
