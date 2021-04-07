522341c1698832c7824dc25fb4116de41a7d90fe
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-04-07 11:51:50 -0400

    switched scripts

--- scan_cifar10.sh
+++ scan_cifar10.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=scanf_cifar10                                 # sets the job name
-#SBATCH --output=scanf_cifar10.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
-#SBATCH --error=scanf_cifar10.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --job-name=scan_cifar10                                 # sets the job name
+#SBATCH --output=scan_cifar10.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=scan_cifar10.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
 #SBATCH --time=24:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --qos=medium                                           # set QOS, this will determine what resources can be requested
 #SBATCH --gres=gpu:2
@@ -11,4 +11,4 @@
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
-srun bash -c "hostname; python scanf.py --config_env configs/env.yml --config_exp configs/scanf/scanf_cifar10.yml --mode train"
+srun bash -c "hostname; python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_cifar10.yml --mode train"
