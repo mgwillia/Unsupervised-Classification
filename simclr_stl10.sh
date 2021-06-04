be9832b2472ad765f5d5e67886518a0d77ba5d1c
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-04 17:40:21 -0400

    trying to fix path

--- simclr_stl10.sh
+++ simclr_stl10.sh
@@ -12,9 +12,11 @@
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
-srun "mkdir -p /scratch0/mgwillia/"
-srun "rsync -r /vulcanscratch/mgwillia/stl10_binary /scratch0/mgwillia/"
+srun "mkdir -p /scratch0/mgwillia;"
+srun "rsync -r /vulcanscratch/mgwillia/stl10_binary.tar.gz /scratch0/mgwillia/;"
+srun "tar zxf /scratch0/mgwillia/stl10_binary.tar.gz;"
 
 srun bash -c "hostname; CUDA_VISIBLE_DEVICES=0 python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_stl10.yml"
 
 srun "rm -r /scratch0/mgwillia/stl10_binary"
+srun "rm /scratch0/mgwillia/stl10_binary.tar.gz"
