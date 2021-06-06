9807923fde844adee8261d6d333c1755c865b8fb
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-06 12:45:06 -0400

    added material for scan

--- scan_cifar10.sh
+++ scan_cifar10.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=scan_cifar10                                 # sets the job name
-#SBATCH --output=scan_cifar10.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
-#SBATCH --error=scan_cifar10.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
-#SBATCH --time=24:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --qos=medium                                           # set QOS, this will determine what resources can be requested
+#SBATCH --job-name=scn_c10                                 # sets the job name
+#SBATCH --output=scn_c10.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=scn_c10.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --time=72:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --account=scavenger
+#SBATCH --partition=scavenger
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:2
-#SBATCH --mem=64G
 #SBATCH --cpus-per-task=8
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
-srun bash -c "hostname; python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_cifar10.yml --mode train"
+srun bash -c "hostname; CUDA_VISIBLE_DEVICES=0,1 python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_cifar10.yml --mode train"
---
+++ scan_stl10.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=scn_stl10                                # sets the job name
+#SBATCH --output=scn_stl10.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=scn_stl10.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --time=72:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --account=scavenger
+#SBATCH --partition=scavenger
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=8
+
+module load cuda/10.0.130                                    # run any commands necessary to setup your environment
+
+srun bash -c "mkdir -p /scratch0/mgwillia;"
+srun bash -c "rsync -r /vulcanscratch/mgwillia/stl10_binary.tar.gz /scratch0/mgwillia/;"
+srun bash -c "tar zxf /scratch0/mgwillia/stl10_binary.tar.gz;"
+
+srun bash -c "hostname; CUDA_VISIBLE_DEVICES=0,1,2,3 python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_stl10.yml --mode train"
+
+srun bash -c "rm -r /scratch0/mgwillia/stl10_binary"
+srun bash -c "rm /scratch0/mgwillia/stl10_binary.tar.gz"
