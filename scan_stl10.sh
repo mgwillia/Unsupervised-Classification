c6f0e79fbc00a053cc9048ebc45166b83f9837e8
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-12 21:39:08 -0400

    standardize copy stl

--- scan_stl10.sh
+++ scan_stl10.sh
@@ -12,10 +12,10 @@
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
-srun bash -c "mkdir -p /scratch0/mgwillia/stl10_binary;"
+srun bash -c "mkdir -p /scratch0/mgwillia;"
 srun bash -c "rsync -r /vulcanscratch/mgwillia/stl10_binary.tar.gz /scratch0/mgwillia/;"
 srun bash -c "tar -zxf /scratch0/mgwillia/stl10_binary.tar.gz -C /scratch0/mgwillia/;"
-srun bash -c "ls /scratch0/mgwillia/*"
+srun bash -c "ls /scratch0/mgwillia/*;"
 
 srun bash -c "hostname; CUDA_VISIBLE_DEVICES=0,1,2,3 python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_stl10.yml --mode train"
 
