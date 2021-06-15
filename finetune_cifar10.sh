480326f528e6f812f9f91afa4a74171e17b20433
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-15 17:28:16 -0400

    batch tweaks

--- finetune_cifar10.sh
+++ finetune_cifar10.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=72:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --partition=scavenger
 #SBATCH --account=scavenger
-#SBATCH --mem=20G
+#SBATCH --mem=64G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 
