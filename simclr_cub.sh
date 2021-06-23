1e829b121824afe9f441f29bb6dbaa29a5452214
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-23 16:28:09 -0400

    asdf

--- simclr_cub.sh
+++ simclr_cub.sh
@@ -3,11 +3,10 @@
 #SBATCH --job-name=simclr_cub                               # sets the job name
 #SBATCH --output=simclr_cub.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
 #SBATCH --error=simclr_cub.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
-#SBATCH --time=72:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --account=scavenger
-#SBATCH --partition=scavenger
+#SBATCH --time=36:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --qos=high
 #SBATCH --gres gpu:p6000:4
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=8
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
