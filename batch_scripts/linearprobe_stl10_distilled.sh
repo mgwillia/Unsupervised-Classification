bf38b1400f428fa8e2fd74144043a996f2dd09e4
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-30 11:22:05 -0400

    stl10

--- batch_scripts/linearprobe_stl10_distilled.sh
+++ batch_scripts/linearprobe_stl10_distilled.sh
@@ -12,4 +12,9 @@
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
+srun bash -c "mkdir -p /scratch0/mgwillia;"
+srun bash -c "rsync -r /vulcanscratch/mgwillia/stl10_binary.tar.gz /scratch0/mgwillia/;"
+srun bash -c "tar -zxf /scratch0/mgwillia/stl10_binary.tar.gz -C /scratch0/mgwillia/;"
+srun bash -c "ls /scratch0/mgwillia/*;"
+
 srun bash -c "hostname; CUDA_VISIBLE_DEVICES=0,1,2,3 python linearprobe.py --config_env configs/env.yml --config_exp configs/linearprobe/linearprobe_stl10_distilled.yml --mode train"
