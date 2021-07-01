17a4c47766b8d18d75a4580f31484602ad6d0df2
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-07-01 11:18:13 -0400

    asdf

--- batch_scripts/mine_imagenet.sh
+++ batch_scripts/mine_imagenet.sh
@@ -12,12 +12,7 @@
 
 module load cuda/10.0.130                                    # run any commands necessary to setup your environment
 
-srun bash -c "mkdir -p /scratch0/mgwillia/imagenet/;"
-srun bash -c "rsync -r /fs/vulcan-datasets/imagenet/ILSVRC2012_img_train.tar /scratch0/mgwillia/imagenet/;"
-srun bash -c "rsync -r /fs/vulcan-datasets/imagenet/ILSVRC2012_img_val.tar /scratch0/mgwillia/imagenet/;"
-srun bash -c "tar -xf /scratch0/mgwillia/imagenet/ILSVRC2012_img_train.tar -C /scratch0/mgwillia/imagenet/;"
-srun bash -c "tar -xf /scratch0/mgwillia/imagenet/ILSVRC2012_img_val.tar -C /scratch0/mgwillia/imagenet/;"
-srun bash -c "ls /scratch0/mgwillia/imagenet;"
-
+srun bash -c "mkdir -p /scratch0/mgwillia"
+srun bash -c "rsync -r /fs/vulcan-datasets/imagenet /scratch0/mgwillia/"
 
 srun bash -c "hostname; CUDA_VISIBLE_DEVICES=0,1,2,3 python mine.py --config_env configs/env.yml --config_exp configs/pretext/mine_imagenet.yml"
